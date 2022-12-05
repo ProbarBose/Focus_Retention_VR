50ba18f0e7064f8b184308ddf6a8b027c1ed8326
Devansh902 <94264624+Devansh902@users.noreply.github.com>
2022-12-06 00:57:37 +0530

    Updated_D_Env

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
-    public GameObject Set01;
-    public GameObject Set02;
-    //public GameObject planeSet02;
-    //public GameObject ballPrefab;
-    //public GameObject[] set01;
-    //public GameObject[] set02;
-    //public GameObject[] prefabShapes;
-    //private IEnumerator fireShapes;
+    public GameObject SpawnSet01;
+    public GameObject SpawnSet02;
+    public GameObject SpawnSet03;
+    public GameObject SpawnSet04;
+    public GameObject SpawnSet05;
+    public GameObject SpawnSet06;
+    public GameObject SpawnSet07;
+    public GameObject SpawnSet08;
+    public GameObject SpawnSet09;
+    public GameObject SpawnSet10;
 
-    //public Transform ballFire;
-    //public float ballForce;
+    public GameObject DisplaySet01;
+    public GameObject DisplaySet02;
+    public GameObject DisplaySet03;
+    public GameObject DisplaySet04;
+    public GameObject DisplaySet05;
+    public GameObject DisplaySet06;
+    public GameObject DisplaySet07;
+    public GameObject DisplaySet08;
+    public GameObject DisplaySet09;
+    public GameObject DisplaySet10;
 
+    public GameObject HoopSet01;
+    public GameObject HoopSet02;
+    public GameObject HoopSet03;
+    public GameObject HoopSet04;
+    public GameObject HoopSet05;
+    public GameObject HoopSet06;
+    public GameObject HoopSet07;
+    public GameObject HoopSet08;
+    public GameObject HoopSet09;
+    public GameObject HoopSet10;
+
+    public GameObject End_Screen;
+    
     private void Start()
     {
         StartCoroutine(fireShapes());
@@ -25,37 +48,135 @@ public class Shooter : MonoBehaviour
     {
         fire01();
         yield return new WaitForSeconds(12f);
-        Destroy(Set01);
+        Destroy(SpawnSet01);
+        Destroy(DisplaySet01);
+        Destroy(HoopSet01);
         fire02();
         yield return new WaitForSeconds(12f);
-        Destroy(Set02);
+        Destroy(SpawnSet02);
+        Destroy(DisplaySet02);
+        Destroy(HoopSet02);
+        fire03();
+        yield return new WaitForSeconds(12f);
+        Destroy(SpawnSet03);
+        Destroy(DisplaySet03);
+        Destroy(HoopSet03);
+        fire04();
+        yield return new WaitForSeconds(12f);
+        Destroy(SpawnSet04);
+        Destroy(DisplaySet04);
+        Destroy(HoopSet04);
+        fire05();
+        yield return new WaitForSeconds(12f);
+        Destroy(SpawnSet05);
+        Destroy(DisplaySet05);
+        Destroy(HoopSet05);
+        fire06();
+        yield return new WaitForSeconds(12f);
+        Destroy(SpawnSet06);
+        Destroy(DisplaySet06);
+        Destroy(HoopSet06);
+        fire07();
+        yield return new WaitForSeconds(12f);
+        Destroy(SpawnSet07);
+        Destroy(DisplaySet07);
+        Destroy(HoopSet07);
+        fire08();
+        yield return new WaitForSeconds(12f);
+        Destroy(SpawnSet08);
+        Destroy(DisplaySet08);
+        Destroy(HoopSet08);
+        fire09();
+        yield return new WaitForSeconds(12f);
+        Destroy(SpawnSet09);
+        Destroy(DisplaySet09);
+        Destroy(HoopSet09);
+        fire10();
+        yield return new WaitForSeconds(12f);
+        Destroy(SpawnSet10);
+        Destroy(DisplaySet10);
+        Destroy(HoopSet10);
+        yield return new WaitForSeconds(4f);
+        End_Screen.SetActive(true);
     }
 
     public void fire01()
     {
-        Set01.SetActive(true);
-        //Instantiate(planeSet01, new Vector3(0, 0, 0), Quaternion.identity);
-        //prefabShapes = new GameObject[set01.Length];
-        //for (int i = 0; i < set01.Length; i++)
-        //{
-        //   prefabShapes[i] = Instantiate(set01[i]) as GameObject;
-        //}
+        DisplaySet01.SetActive(true);
+        SpawnSet01.SetActive(true);
+        HoopSet01.SetActive(true);
         
-        //int n = instantiatedShapes.Length;
-        //GameObject multiBall = Instantiate(instantiatedShapes[n]);
-        //Rigidbody rb = multiBall.GetComponent<Rigidbody>();
-        //rb.AddForce(ballFire.forward * ballForce, ForceMode.Impulse);
-        //Destroy(multiBall, 3f);
     }
     public void fire02()
     {
-        Set02.SetActive(true);
-        //Instantiate(planeSet01, new Vector3(0, 0, 0), Quaternion.identity);
-        /*prefabShapes = new GameObject[set02.Length];
-        for (int i = 0; i < set02.Length; i++)
-        {
-            prefabShapes[i] = Instantiate(set02[i]) as GameObject;
-        }*/
+        DisplaySet02.SetActive(true);
+        SpawnSet02.SetActive(true);
+        HoopSet02.SetActive(true);
+
+    }
+
+    public void fire03()
+    {
+        DisplaySet03.SetActive(true);
+        SpawnSet03.SetActive(true);
+        HoopSet03.SetActive(true);
+
+    }
+
+    public void fire04()
+    {
+        DisplaySet04.SetActive(true);
+        SpawnSet04.SetActive(true);
+        HoopSet04.SetActive(true);
+
+    }
+
+    public void fire05()
+    {
+        DisplaySet05.SetActive(true);
+        SpawnSet05.SetActive(true);
+        HoopSet05.SetActive(true);
+
+    }
+
+    public void fire06()
+    {
+        DisplaySet06.SetActive(true);
+        SpawnSet06.SetActive(true);
+        HoopSet06.SetActive(true);
+
+    }
+
+    public void fire07()
+    {
+        DisplaySet07.SetActive(true);
+        SpawnSet07.SetActive(true);
+        HoopSet07.SetActive(true);
+
+    }
+
+    public void fire08()
+    {
+        DisplaySet08.SetActive(true);
+        SpawnSet08.SetActive(true);
+        HoopSet08.SetActive(true);
+
+    }
+
+    public void fire09()
+    {
+        DisplaySet09.SetActive(true);
+        SpawnSet09.SetActive(true);
+        HoopSet09.SetActive(true);
+
+    }
+
+    public void fire10()
+    {
+        DisplaySet10.SetActive(true);
+        SpawnSet10.SetActive(true);
+        HoopSet10.SetActive(true);
+
     }
 
 }
